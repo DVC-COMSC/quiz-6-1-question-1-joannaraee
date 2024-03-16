b83bffd9a9d1f468975186131f3b24122055ac39
joannaraee <joannapalmer54@gmail.com>
2024-03-15 18:35:49 -0700

    quiz-6-1-question-1

--- main.hpp
+++ main.hpp
@@ -1,8 +1,21 @@
 
-//********************
-// Declare Function Prototypes
-//********************
+#include <iostream> 
+#include <vector> 
 
-// ******************************
-// Implement all your functions here
-// ******************************
+void getTwoValues(int &begin, int &end); 
+void getNextPrime(int begin); 
+void getPrevPrime(int end); 
+
+bool isPrime(int n) {
+    if (n <= 1)
+        return false; 
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return false; 
+    }
+    return true; 
+}
+
+
+
+
